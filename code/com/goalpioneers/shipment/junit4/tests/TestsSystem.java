4fb00458540126159571256cfe04d0fc3f306cd3
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-07-15 11:35:37 +0200

    added junit4 test

---
+++ code/com/goalpioneers/shipment/junit4/tests/TestsSystem.java
@@ -0,0 +1,14 @@
+package com.goalpioneers.shipment.junit4.tests;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestsSystem 
+{
+	
+	@Test
+	public void testPrint()
+	{
+		assertEquals(true, false);
+	}
+}
