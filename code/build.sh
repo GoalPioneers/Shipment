8920d3aa69711dd8f0d191945abaec30e08ce19e
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-05 17:46:45 +0200

    maven workflow

--- code/build.sh
+++ code/build.sh
@@ -6,5 +6,5 @@ mvn package
 jar uvfm target/Shipment-1.0.0.alpha.snapshot.jar manifest.mf
 
 # Execute
-java -jar target/Shipment-1.0.0.alpha.snapshot.jar
+#java -jar target/Shipment-1.0.0.alpha.snapshot.jar
 #java -cp target/Shipment-1.0.0.alpha.snapshot.jar main.java.com.goalpioneers.shipment.PublicEntry
