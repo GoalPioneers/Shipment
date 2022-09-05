d1ed7da37b82e973b3407166ae91894312041c25
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-05 21:04:52 +0200

    adding commands

--- code/src/main/java/com/goalpioneers/shipment/commands/CommandLine.java
+++ code/src/main/java/com/goalpioneers/shipment/commands/CommandLine.java
@@ -1,7 +1,42 @@
-package main.java.com.goalpioneers.shipment.commands;
-
-
-public class CommandLine
-{
-
-}
+	package main.java.com.goalpioneers.shipment.commands;
+	
+	import java.util.ArrayList;
+	import java.util.List;
+	
+	
+	public abstract class CommandLine
+	{
+		public CommandLine( String key )
+		{
+			this( key, new ArrayList <CommandOption>() );
+		}
+		
+		public CommandLine( String key, List<CommandOption> options)
+		{
+			this.setKey( key );
+			this.setOptions( options );
+		}
+		
+		private String key = null;
+		private List <CommandOption> options = null;
+		
+		public List <CommandOption> getOptions()
+		{
+			return this.options;
+		}
+		
+		public String getKey()
+		{
+			return this.key;
+		}
+		
+		public void setOptions( List <CommandOption> options )
+		{
+			this.options = options;
+		}
+		
+		public void setKey( String key )
+		{
+			this.key = key;
+		}
+	}
---
+++ code/src/main/java/com/goalpioneers/shipment/commands/repositories/update/UpdateCommand.java
@@ -0,0 +1,7 @@
+package main.java.com.goalpioneers.shipment.commands.repositories.update;
+
+
+public class UpdateCommand
+{
+
+}
