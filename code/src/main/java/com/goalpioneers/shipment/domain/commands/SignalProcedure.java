3a9db07919b8f36a4587d177c1616111a8a6810f
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-05 15:23:36 +0200

    working on maven setup

---
+++ code/src/main/java/com/goalpioneers/shipment/domain/commands/SignalProcedure.java
@@ -0,0 +1,20 @@
+package main.java.com.goalpioneers.shipment.domain.commands;
+
+
+public abstract class SignalProcedure
+{
+    // Variables
+    private SignalCommand command = SignalCommand.None;
+
+
+    // Accessors
+    public SignalCommand getCommand()
+    {
+        return this.command;
+    }
+
+    public void setCommand( SignalCommand command )
+    {
+        this.command = command;
+    }
+}
