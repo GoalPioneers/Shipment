b137e606aac55b382427e26da6909e681e308549
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-05 17:35:57 +0200

    can build files now

--- code/test.sh
+++ code/test.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
 mvn compile --debug
-
-# Updates the manifest
-jar uvfm target/Shipment-1.0.0.alpha.snapshot.jar manifest.mf
-
-# Execute
-java -jar target/Shipment-1.0.0.alpha.snapshot.jar
-#java -cp target/Shipment-1.0.0.alpha.snapshot.jar main.java.com.goalpioneers.shipment.PublicEntry
