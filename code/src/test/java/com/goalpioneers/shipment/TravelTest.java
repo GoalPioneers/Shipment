75333272931dc2f319609094407c1eed2c53d630
Kent Madsen <kent.vejrup.madsen@designermadsen.dk>
2022-09-06 11:12:40 +0200

    removing test

--- code/src/test/java/com/goalpioneers/shipment/TravelTest.java
+++ code/src/test/java/com/goalpioneers/shipment/TravelTest.java
@@ -2,12 +2,10 @@ package test.java.com.goalpioneers.shipment;
 
 
 import main.java.com.goalpioneers.shipment.storage.algorithms.TravelStorage;
-import org.junit.jupiter.api.Test;
 
 
 public class TravelTest
 {
-	@Test
 	void search()
 	{
 		String pathRoot = "/home/madsen/dataset/custom001";
